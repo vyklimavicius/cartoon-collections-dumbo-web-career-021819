f0dac601c99ac5710a8de13f29bb55356d62a846
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-02-01 16:19:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |word|
-    word.include?(cheese_types)
-  end 
+    word == "cheddar" || word == "gouda" || word == "camembert"
+  end
 end
