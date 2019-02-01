bc02067ffd606502a5d6a301aec3f3f3894121d8
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-02-01 16:03:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,7 @@
 def roll_call_dwarves(array)
   array.each_with_index do |name,idx|
-    puts ""
-      
+    puts "#{idx} #{name}"
   end 
-  
 end
 
 def summon_captain_planet# code an argument here
